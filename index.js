5635904c4dba568e6cc23684ced668a722ce6d51
wazquez <victorvazquezv@gmail.com>
2026-03-29 16:08:01 +0200

    Updates

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const BACKEND_URL = "http://localhost:3000";
+// const BACKEND_URL = "http://localhost:3000";
+const BACKEND_URL = "https://258d-2a0c-5a84-e609-a00-84d4-e94e-1551-7d59.ngrok-free.app";
 const WXCC_HOOK_URL = "https://hooks.us.webexconnect.io/events/12IOCZHHTT";
 const VIDEO_DESTINATION = new URLSearchParams(window.location.search).get("destination");
 
