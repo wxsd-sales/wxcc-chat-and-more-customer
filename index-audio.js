2beee8e8bca34db46313cc7167dcab724a192999
wazquez <victorvazquezv@gmail.com>
2026-05-11 22:42:53 +0200

    Updates

--- index-audio.js
+++ index-audio.js
@@ -1,7 +1,7 @@
 // const BACKEND_URL = "http://localhost:3000";
 const BACKEND_URL = "https://be-guest-and-meeting-creation-production.up.railway.app";
 const VIDEO_DESTINATION = new URLSearchParams(window.location.search).get("destination");
-const CUSTOMER_EMAIL = "vvazquez@wxsd.us";
+const CUSTOMER_EMAIL = "vvazquez@cxocoe.us";
 const WXCC_HOOK_URL = "https://hooks.us.webexconnect.io/events/HILBRZW77M";
 const INAPP_APP_ID = "DA05221332";
 const INAPP_USER_ID = "6806ea7s-a04e-4fdb-9d86-0b33626f3577";
